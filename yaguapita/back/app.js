8150a4bdc8fdab70841be2b25a21474cd206fb15
natalia colangelo <naticolangelo8@gmail.com>
2023-12-06 10:39:52 -0300

    yaguapita

--- yaguapita/back/app.js
+++ yaguapita/back/app.js
@@ -73,6 +73,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.use('/admin/novedades',secured, adminNovedadesRouter);
+
 
 module.exports = app;
--- yaguapita/back/models/usuariosModel.js
+++ yaguapita/back/models/usuariosModel.js
@@ -4,7 +4,7 @@ var md5 = require('md5');
 async function getUserByUsernameAndPassword(user, password) {
     try{
         var query = "select * from usuarios where usuario = ? limit 1";
-        var rows = await pool.queryI(query, [user, md5(password) ]);
+        var rows = await pool.queryI(query,[user] );
         return rows[0];
     } catch (error){
         throw error;
